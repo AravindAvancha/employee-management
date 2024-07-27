ac8496ad6332f08d9e50142a1ba098f0b853114a
Avancha Aravind <avanchaaravind9@gmail.com>
2024-07-27 09:38:19 +0530

    sending to remote repository

---
+++ react-work-space/react_frontend/src/components/FooterComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+	export default class FooterComponent extends Component {
+ 	 render() {
+    	return (
+      	<div>
+        	<footer className='footer'>
+       <span className>All Right Reserved 2024 &copy;Aravind Avancha</span>
+        </footer>
+     	 </div>
+    	)
+  	}
+	}
